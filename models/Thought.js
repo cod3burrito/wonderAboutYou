d92b6ddf6d289c43389f2bfe40912227a4c1f8de
cod3burrito <robintanner44@gmail.com>
2022-12-12 13:38:36 -0500

    models written

---
+++ models/Thought.js
@@ -0,0 +1,33 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/index.js');
+const reactionSchema = require('./Reaction.js');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    { toJSON: { getters: true } }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
